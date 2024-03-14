6afe45b97eaae21f525cb8ea556f18f9cfaacc10
athirapallath <athirapallath.in@gmail.com>
2024-03-13 21:25:03 -0400

    Spring boot transaction

---
+++ src/main/java/com/javainuse/SpringBootJdbcApplication.java
@@ -0,0 +1,41 @@
+package com.javainuse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.javainuse.model.Employee;
+import com.javainuse.model.EmployeeHealthInsurance;
+import com.javainuse.service.EmployeeService;
+import com.javainuse.service.OrganizationService;
+
+@SpringBootApplication
+public class SpringBootJdbcApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringBootJdbcApplication.class, args);
+		OrganizationService organizationService = context.getBean(OrganizationService.class);
+
+		Employee emp = new Employee();
+		emp.setEmpId("emp1");
+		emp.setEmpName("emp1");
+
+		EmployeeHealthInsurance employeeHealthInsurance = new EmployeeHealthInsurance();
+		employeeHealthInsurance.setEmpId("emp1");
+		employeeHealthInsurance.setHealthInsuranceSchemeName("premium");
+		employeeHealthInsurance.setCoverageAmount(20000);
+
+		Employee emp2 = new Employee();
+		emp.setEmpId("emp2");
+		emp.setEmpName("emp2");
+
+		EmployeeHealthInsurance employeeHealthInsurance2 = new EmployeeHealthInsurance();
+		employeeHealthInsurance.setEmpId("emp2");
+		employeeHealthInsurance.setHealthInsuranceSchemeName("premium");
+		employeeHealthInsurance.setCoverageAmount(40000);
+
+		organizationService.joinOrganization(emp, employeeHealthInsurance);
+
+	}
+}
